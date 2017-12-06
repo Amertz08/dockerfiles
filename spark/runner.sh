1ab06059619109841363616dd825d8c834e4b629
Adam Mertz <adammertz@gmail.com>
2017-12-06 01:40:08 -0600

    Runner script runs nodes w/o shutting down container

--- spark/runner.sh
+++ spark/runner.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
+CLASS=org.apache.spark.deploy
+
 usage () {
   echo "USAGE"
-  echo "py : run pyspark"
+  echo "py <MASTER_HOST> : run pyspark"
   echo "master : run master node"
-  echo "slave <MASTER_HOST_IP/NAME>:<PORT> : run slave node"
+  echo "slave <MASTER_HOST>  : run slave node"
+}
+
+load_config () {
+    . $SPARK_HOME/sbin/spark-config.sh
+    . $SPARK_HOME/bin/load-spark-env.sh
+}
+
+run_py () {
+    pyspark --master spark://$1:$SPARK_MASTER_PORT
+}
+
+run_master () {
+    load_config
+    $SPARK_HOME/bin/spark-class $CLASS.master.Master \
+    -h $(hostname) --port $SPARK_MASTER_PORT \
+    --webui-port $SPARK_MASTER_WEBUI_PORT >> $LOG_DIR/$LOG_FILE
+}
+
+run_worker () {
+    load_config
+    $SPARK_HOME/bin/spark-class $CLASS.worker.Worker \
+    --webui-port $SPARK_MASTER_WEBUI_PORT $1:$SPARK_MASTER_PORT >> $LOG_DIR/$LOG_FILE
 }
 
 case $1 in
   (py)
-    pyspark
+    run_py $2
     ;;
   (master)
-    echo "master"
-    $SPARK_HOME/sbin/start-master.sh &
+    run_master
     ;;
-  (slave)
-    echo "slave"
-    $SPARK_HOME/sbin/start-slave.sh $2 &
+  (worker)
+    run_worker $2
     ;;
   (*)
     usage
